03430e78618253a7fe627c1aef162e0af8addfe8
Ashley Osborne <ashley.osborne@prs-im.co.uk>
2025-11-25 13:33:32 +0000

    Set GDPR checkbox to checked by default

--- premarketing/urls.py
+++ premarketing/urls.py
@@ -3,10 +3,8 @@ URL configuration for Bruce pre-marketing site.
 """
 from django.contrib import admin
 from django.urls import path, include
-from signup.views import honeypot_admin
 
 urlpatterns = [
-    path('centralmanagementserver/', admin.site.urls),  # Real admin
-    path('admin/', honeypot_admin),  # Honeypot - fake admin
+    path('admin/', admin.site.urls),
     path('', include('signup.urls')),
 ]
