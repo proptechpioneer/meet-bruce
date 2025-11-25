76fa9fd79a7fb5eb25c78ea30fb88a51b8ea279c
Ashley Osborne <ashley.osborne@prs-im.co.uk>
2025-11-25 12:37:44 +0000

    Add /lander path for landing page

--- signup/urls.py
+++ signup/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('lander/', views.index, name='lander'),  # Main landing page
     path('signup/', views.signup_view, name='signup'),
 ]
